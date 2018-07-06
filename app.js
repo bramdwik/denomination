456a603aba3a2f824abc65e0f665c0c54251565c
Bram Dwi Kurniaji <bramdk121@gmail.com>
2018-07-06 10:05:45 +0700

    refactor better solution

--- app.js
+++ app.js
@@ -8,36 +8,55 @@ const sumAll = (arr) => {
   }
 }
 
-const value = 25
+const value = 27
 
 const denomination = [1, 3, 5]
 
 let solution = []
 let i = Math.min(...denomination)
+let loopCounter = 0
 for (i; i <= value; i++) {
-  solution[i] = []
+  loopCounter++
   if (denomination.includes(i)) {
     solution[i] = [i]
   } else {
-    let j = Math.min(...denomination)
-    for (j; j <= i; j++) {
-      let tempSolution = []
-      if (solution[j - 1]) {
-        denomination.forEach(x => {
-          const sum = sumAll(solution[j - 1])
-          const total = sum + x
-          if (total === i) {
-            tempSolution = solution[j - 1].concat([x])
-            if (solution[i].length === 0 || tempSolution.length < solution[i].length) {
-              solution[i] = tempSolution
-            }
+    // Solution 1
+    // let j = Math.min(...denomination)
+    // for (j; j <= i; j++) {
+    //   let tempSolution = []
+    //   if (solution[j - 1]) {
+    //     denomination.forEach(x => {
+    //       const sum = sumAll(solution[j - 1])
+    //       const total = sum + x
+    //       if (total === i) {
+    //         tempSolution = solution[j - 1].concat([x])
+    //         if (solution[i].length === 0 || tempSolution.length < solution[i].length) {
+    //           solution[i] = tempSolution
+    //         }
+    //       }
+    //     })
+    //   }
+    // }
+
+    // Solution 2
+    denomination.forEach(x => {
+      loopCounter++
+      const sum = sumAll(solution[i - 1]) + x
+      const tempResult = solution[i - 1].concat([x])
+      if (sum <= value) {
+        if (typeof solution[sum] === 'undefined') {
+          solution[sum] = tempResult
+        } else {
+          if (solution[sum].length > tempResult.length) {
+            solution[sum] = tempResult
           }
-        })
+        }
       }
-    }
+    })
     if (solution[i].length === 0) {
       console.log('Value ' + i + ' has no solution.')
     }
   }
 }
 console.log(solution[value])
+console.log(loopCounter)
